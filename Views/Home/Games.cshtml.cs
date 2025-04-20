d8ce68b3f0988b0429e3d92179eddc19472448f6
Bradley-Laubscher <bradleylaubscher@gmail.com>
2025-04-20 15:46:11 +0200

    Updated home page styling.

---
+++ Views/Home/Games.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NextUp.Views.Home
+{
+    public class GamesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
